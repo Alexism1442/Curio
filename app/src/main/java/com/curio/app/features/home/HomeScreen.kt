bf932c09e3cfd1eb9a593e98c106deea5981cea2
firefly <khanquadir19@gmail.com>
2026-08-07 10:16:05 +0000

    fix: import widthIn modifier in HomeScreen (CI compile)

--- app/src/main/java/com/curio/app/features/home/HomeScreen.kt
+++ app/src/main/java/com/curio/app/features/home/HomeScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
